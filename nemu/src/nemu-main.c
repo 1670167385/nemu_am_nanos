c88835a4c3a32df3264762ace252d83c18a57d81
tracer-ics2021 <tracer@njuics.org>
2022-02-27 14:45:49 +0800

    >  compile 2054343    Static hostname: xsr-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6b0fafce6b65411598b2e7389dc32119            Boot ID: 36cb1298be5d4e42b2f925f85a2d7429   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-49-generic       Architecture: x86-64  14:45:49 up 1 day, 50 min,  1 user,  load average: 0.03, 0.06, 0.08

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -8,19 +8,6 @@ int is_exit_status_bad();
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 
-for(int i=0; i < argc; i++)
-{
-	char tmp[65536] = {0};
-	word_t ans=0 ,exout;
-	sscanf(argv[i],"%s",tmp);
-	bool sucs = true;
-	exout = expr(tmp, &sucs);
-	if(sucs)
-		printf("%d %d %d\n",ans,exout,exout == ans);
-	else
-		printf("wrong\n");
-}
-/*
 
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
@@ -28,13 +15,23 @@ for(int i=0; i < argc; i++)
   init_monitor(argc, argv);
 #endif
 
-*/
 
   /* Start engine. */
-  /*
-	engine_start();
+	for(int i=0; i < argc; i++)
+	{
+		char tmp[65536] = {0};
+		word_t ans=0 ,exout;
+		sscanf(argv[i],"%s",tmp);
+		bool sucs = true;
+		exout = expr(tmp, &sucs);
+		if(sucs)
+			printf("%d %d %d\n",ans,exout,exout == ans);
+		else
+			printf("wrong\n");
+	}
+
+	//engine_start();
 
   return is_exit_status_bad();
-	*/
-	return 0;
+	
   }
