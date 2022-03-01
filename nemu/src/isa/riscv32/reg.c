07f8addace4afbc8864e11fe3c78d931eeaef599
tracer-ics2021 <tracer@njuics.org>
2022-03-01 15:42:26 +0800

    >  compile 2054343    Static hostname: xsr-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 6b0fafce6b65411598b2e7389dc32119            Boot ID: 3612ea643023460bb1276c369b040ff5   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-49-generic       Architecture: x86-64  15:42:26 up 57 min,  1 user,  load average: 0.26, 0.14, 0.13

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -12,10 +12,20 @@ const char *regs[] = {
 void isa_reg_display() {
 	for(int i=0;i<REGS_LEN;i++){
 		printf("%-15s%-15x%-15d\n",regs[i],cpu.gpr[i]._32,cpu.gpr[i]._32);
-	}
+	 }
 	printf("%-15s%-15x%-15d\n","pc",cpu.pc,cpu.pc);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
-  return 0;
+	*success = true;
+	if(strcmp(s, "pc") == 0)
+		return cpu.pc;
+	for(int i=0;i<REGS_LEN;i++){
+		if(strcmp(s,regs[i]) == 0)
+		{
+			return cpu.gpr[i]._32;
+		}
+	}
+	*success = false;
+  	return 0;
 }
