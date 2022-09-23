46f98e86c6e8c7ad85bd9a76e59179ff21c466f6
tracer-ics2021 <tracer@njuics.org>
2022-09-23 16:14:02 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 71b2a6c86d244688ae96c35319b4305e Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  16:14:02 up 14:24,  1 user,  load average: 0.66, 0.69, 0.60

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -15,9 +15,9 @@ int main(int argc, char *argv[]) {
 		int ans;
 		sscanf(argv[i * 2 + 1],"%d",&ans);
 		int output;
-		bool sucs;
+		//bool sucs=1;
 		printf("%s\n",argv[i * 2 + 2]);
-		output = expr(argv[i * 2 + 2], &sucs);
+		//output = expr(argv[i * 2 + 2], &sucs);
 		printf("ans:%d output:%d \n", ans, output);
 	}
 	return 0;
