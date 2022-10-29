a98e2279d1b4ae9b32747d2fa34732f8fa477065
tracer-ics2021 <tracer@njuics.org>
2022-10-29 18:52:19 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: fdc91c9a15d64b6894971f47be7e3c5d Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  18:52:19 up  5:30,  1 user,  load average: 1.37, 0.56, 0.27

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -10,6 +10,20 @@ def_EHelper(sub) {
   rtl_sub(s, ddest, dsrc1, dsrc2);
 }
 
+def_EHelper(slt) {
+  if(*dsrc1 < *dsrc2)
+    rtl_li(s, ddest, 1);
+  else
+    rtl_li(s, ddest, 0);
+}
+
+def_EHelper(sltu) {
+  if((uint32_t)*dsrc1 < (uint32_t)*dsrc2)
+    rtl_li(s, ddest, 1);
+  else
+    rtl_li(s, ddest, 0);
+}
+
 def_EHelper(addi) {
   rtl_addi(s, ddest, dsrc1, id_src2->simm);
 }
