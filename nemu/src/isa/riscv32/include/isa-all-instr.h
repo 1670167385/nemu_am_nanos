96ac7861d5d3bc058bdfc6e66f3d0455f4078d6a
tracer-ics2021 <tracer@njuics.org>
2022-10-29 19:23:23 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: fdc91c9a15d64b6894971f47be7e3c5d Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  19:23:23 up  6:01,  1 user,  load average: 0.99, 1.17, 1.06

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -2,6 +2,7 @@
 #include "../local-include/rtl.h"
 
 #define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap) f(addi) f(auipc) f(jal) f(jalr) \
-                      f(beq) f(sltiu) f(slti) f(bne) f(add) f(sub) f(slt) f(sltu) f(xor) f(or)
+                      f(beq) f(sltiu) f(slti) f(bne) f(add) f(sub) f(slt) f(sltu) f(xor) f(or)\
+                      f(sh)
 
 def_all_EXEC_ID();
