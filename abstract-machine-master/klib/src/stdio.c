7be249b8e6eb5c12d6b932d6fef404b0dacdfa52
tracer-ics2021 <tracer@njuics.org>
2022-11-06 00:58:03 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 23eaebc34524482db8f8af91670bfcf0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  00:58:03 up 1 day,  1:13,  8 users,  load average: 0.62, 0.59, 0.46

--- abstract-machine-master/klib/src/stdio.c
+++ abstract-machine-master/klib/src/stdio.c
@@ -14,7 +14,48 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  size_t p = 0,pf = 0;
+
+  va_list ap;
+  int d, n = 0;
+  int tmp_d;
+  char *s;
+ 
+  va_start(ap, fmt);
+  while(*fmt) {
+    while(*(char*)fmt != '%'){
+      out[p++] = *(char*)fmt;
+      fmt++;
+    }
+    switch(*(++fmt)) {
+      case 's':  /*string*/
+        n++;
+        s = va_arg(ap, char *);
+        while(*(char*)s != '\0'){
+          out[p++] = *(char*)s;
+          s++;
+        }
+        break;
+      case 'd':  /*int*/
+        n++;
+        d = va_arg(ap, int);
+        tmp_d = d;
+        while(tmp_d){
+          p++;
+          tmp_d/=10;
+        }
+        pf = p;
+        while(d){
+          out[--p] = (d%10) + '0';
+          d/=10;
+        }
+        p = pf;
+        break;
+    }
+    fmt++;
+  }
+  va_end(ap);
+  return n;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine-master/klib/src/string.c
+++ abstract-machine-master/klib/src/string.c
@@ -5,43 +5,83 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t p = 0;
+  while(s[p] != '\0')
+    p++;
+  return p;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t p = 0;
+  while(src[p] != '\0') {
+    dst[p] = src[p];
+    p++;
+  }
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
-}
+  size_t p = 0;
+  while(src[p] != '\0' && p<n) {
+    dst[p] = src[p];
+    p++;
+  }
+  return dst;
+} 
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
-}
+  size_t p = 0, s = strlen(dst);
+  while(src[p] != '\0') dst[s++] = src[p++];
+  return dst;
+} 
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  size_t p =0;
+  while(s1[p]==s2[p]&&s1[p]!='\0')p++;
+  return (s1[p] - s2[p]);
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t p =0;
+  while(s1[p]==s2[p]&&s1[p]!='\0'&&p<n)p++;
+  return (s1[p-1] - s2[p-1]);
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  size_t p =0;
+  while(p<n){
+    *((char*)s + p) = c;
+    p++;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  if(dst < src || dst >= src + n){
+    memcpy(dst, src, n);
+  }
+  else {
+    while(n--){
+      *((char*)dst + n) = *((char*)src + n);
+    }
+  }
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  size_t p=0;
+  while(p<n)
+  {
+      *((char*)out + p) = *((char*)in + p);
+      p++;
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  size_t p =0;
+  while(*((char*)s1 + p) == *((char*)s2 + p)&& p<n)p++;
+  return (*((char*)s1 + p) - *((char*)s2 + p));
 }
 
 #endif
