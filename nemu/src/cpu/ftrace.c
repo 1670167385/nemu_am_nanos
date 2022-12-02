fa835e55805c764ee3a53bb9b874f581041bcbd4
tracer-ics2021 <tracer@njuics.org>
2022-12-02 14:42:44 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  14:42:44 up 1 day, 16:18,  1 user,  load average: 1.08, 0.91, 0.75

--- nemu/src/cpu/ftrace.c
+++ nemu/src/cpu/ftrace.c
@@ -15,19 +15,18 @@ void init_FTRACE(const char* elf_file)
     Elf32_Ehdr ehdr;
     Elf32_Shdr *shdr;
     int fread_ret;
-    //read Ehearder
+    /* read Ehearder */
     fread_ret = fread(&ehdr, sizeof(ehdr), 1, fp);
 
     if(ehdr.e_shoff)
         fseek(fp, ehdr.e_shoff, SEEK_SET);
     assert(ehdr.e_shoff);
-    //read section header , number is e_shnum
+    /* read section header , number is e_shnum */
     shdr = malloc(sizeof(Elf32_Shdr)*ehdr.e_shnum);
     assert(shdr);
     fread_ret = fread(shdr, sizeof(Elf32_Shdr), ehdr.e_shnum, fp);
 
-    Log("has opened it ");
-    //read string table
+    /* read string table */
     char *stringtb;
     stringtb = malloc(sizeof(char)*shdr[ehdr.e_shstrndx].sh_size);
     assert(stringtb);
@@ -36,10 +35,13 @@ void init_FTRACE(const char* elf_file)
     fseek(fp, shdr[ehdr.e_shstrndx].sh_offset, SEEK_SET);
     fread_ret = fread(stringtb, sizeof(stringtb), 1, fp);
     assert(fread_ret == 1);
+    for(int i= 0;i<shdr[ehdr.e_shstrndx].sh_size;i++)
+        printf("%s\n",stringtb+i);
+
+
     for(int i = 0;i<ehdr.e_shnum;i++)
-    {
-        printf("[%d]%s: 0x%x\n", i,&stringtb[shdr[i].sh_name], shdr[i].sh_addr);
-    }
+        printf("[%d]%s: 0x%x\n", i, &stringtb[shdr[i].sh_name], shdr[i].sh_addr);
+    
   }
 //#endif
 }
