6aab93cc5e283f2ab29c30e88b33a6eaf46de0d4
tracer-ics2021 <tracer@njuics.org>
2022-12-02 19:12:06 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  19:12:06 up 1 day, 20:47,  1 user,  load average: 1.37, 1.12, 1.06

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -75,10 +75,10 @@ static void fetch_decode_exec_updatepc(Decode *s)
     s->EHelper(s);
     cpu.pc = s->dnpc;
 //#ifdef CONFIG_FTRACE
-    if(s->EHelper == exec_jal || (s->EHelper == exec_jalr && s->src2.preg != &cpu.gpr[1]._32)){
+    if(s->EHelper == exec_jal || (s->EHelper == exec_jalr && s->src1.preg != &cpu.gpr[1]._32)){
         ftrace_call(s->pc, cpu.pc);
     }
-    if(s->EHelper == exec_jalr && s->src2.preg == &cpu.gpr[1]._32){
+    if(s->EHelper == exec_jalr && s->src1.preg == &cpu.gpr[1]._32){
         ftrace_ret(s->pc, cpu.pc);
     }
 //#endif
