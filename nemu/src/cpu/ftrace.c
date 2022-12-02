2617b070fb7076bd723432898bed7e3c5e3689f8
tracer-ics2021 <tracer@njuics.org>
2022-12-02 16:04:22 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  16:04:22 up 1 day, 17:39,  1 user,  load average: 1.08, 1.14, 1.06

--- nemu/src/cpu/ftrace.c
+++ nemu/src/cpu/ftrace.c
@@ -4,7 +4,11 @@
 #include <elf.h>
 
 
-
+typedef struct {
+    char name[25];
+    int func_st;
+    int func_end;
+}FUNCT;
 
 void init_FTRACE(const char* elf_file)
 {
@@ -26,23 +30,61 @@ void init_FTRACE(const char* elf_file)
     assert(shdr);
     fread_ret = fread(shdr, sizeof(Elf32_Shdr), ehdr.e_shnum, fp);
 
-    /* read string table */
+    /* read section string table */
+    //char *stringtb;
+    //stringtb = malloc(sizeof(char)*shdr[ehdr.e_shstrndx].sh_size);
+    //assert(stringtb);
+
+    //assert(ehdr.e_shstrndx != SHN_UNDEF);//has no string table
+    //fseek(fp, shdr[ehdr.e_shstrndx].sh_offset, SEEK_SET);
+    //fread_ret = fread(stringtb, shdr[ehdr.e_shstrndx].sh_size, 1, fp);
+    //assert(fread_ret == 1);
+
+    /* print section table*/
+    //for(int i = 0;i<ehdr.e_shnum;i++)
+    //    printf("[%d]%s: 0x%x\n", i, &stringtb[shdr[i].sh_name], shdr[i].sh_offset);
+
+    /* read symbol string */ 
+    int symtab;
+    for(symtab = 0; symtab<ehdr.e_shnum && shdr[symtab+1].sh_type!=SHT_SYMTAB ;symtab++);
     char *stringtb;
-    stringtb = malloc(sizeof(char)*shdr[ehdr.e_shstrndx].sh_size);
+    stringtb = malloc(sizeof(char)*shdr[symtab+1].sh_size);
     assert(stringtb);
 
-    assert(ehdr.e_shstrndx != SHN_UNDEF);//has no string table
-    fseek(fp, shdr[ehdr.e_shstrndx].sh_offset, SEEK_SET);
-    printf("%lx",sizeof(stringtb));
-    fread_ret = fread(stringtb, shdr[ehdr.e_shstrndx].sh_size, 1, fp);
+    fseek(fp, shdr[symtab+1].sh_offset, SEEK_SET);
+    fread_ret = fread(stringtb, shdr[symtab+1].sh_size, 1, fp);
     assert(fread_ret == 1);
-    //for(int i= 0;i<shdr[ehdr.e_shstrndx].sh_size;i++)
-      //  printf("%s\n",stringtb+i);
 
+    /* read symbol table */
+    Elf32_Sym *sym = malloc(shdr[symtab].sh_size*sizeof(char));
+    assert(sym);
+    fseek(fp, shdr[symtab].sh_offset, SEEK_SET);
+    fread_ret = fread(sym, shdr[symtab].sh_size, 1, fp);
+    assert(fread_ret == 1);
 
-    for(int i = 0;i<ehdr.e_shnum;i++)
-        printf("[%d]%s: 0x%x\n", i, &stringtb[shdr[i].sh_name], shdr[i].sh_addr);
-    
+    /* alloc func_table*/
+    int func_num=0, sym_num = shdr[symtab].sh_size/sizeof(Elf32_Sym);
+    for(int i=0;i<sym_num;i++)
+        if(sym[i].st_info == STT_FUNC)
+            func_num++;
+    FUNCT *func_table = malloc(sizeof(FUNCT)*func_num);
+    assert(func_table);
+
+    /* store in functable */
+    int p=0;
+    for(int i=0;i<sym_num;i++)
+        if(sym[i].st_info == STT_FUNC)
+        {
+            strcpy(func_table[p].name, &stringtb[sym[i].st_name]);
+            func_table[p].func_st = sym[i].st_value;
+            func_table[p].func_end = sym[i].st_size;
+            printf("%d\t%s\t:%d\t%d\n", i, func_table[p].name, func_table[p].func_st, func_table[p].func_end);
+            p++;
+        }
+
+    free(shdr);
+    free(stringtb);
+    free(sym);
   }
 //#endif
 }
