3fce9d857f9ebfe74d212754c0feee444f867564
tracer-ics2021 <tracer@njuics.org>
2022-12-02 14:17:56 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  14:17:56 up 1 day, 15:53,  1 user,  load average: 0.57, 0.62, 0.70

--- nemu/src/cpu/ftrace.c
+++ nemu/src/cpu/ftrace.c
@@ -18,7 +18,6 @@ void init_FTRACE(const char* elf_file)
     //read Ehearder
     fread_ret = fread(&ehdr, sizeof(ehdr), 1, fp);
 
-    Log("has opened it ");
     if(ehdr.e_shoff)
         fseek(fp, ehdr.e_shoff, SEEK_SET);
     assert(ehdr.e_shoff);
@@ -31,6 +30,7 @@ void init_FTRACE(const char* elf_file)
     fseek(fp, shdr[ehdr.e_shstrndx].sh_offset, SEEK_SET);
     fread_ret = fread(&stringtb, sizeof(char), shdr[ehdr.e_shstrndx].sh_size, fp);
     assert(fread_ret == 1);
+    Log("has opened it ");
     for(int i = 0;i<ehdr.e_shnum;i++)
     {
         printf("[%d]%sq: 0x%x\n", i, &stringtb[shdr[i].sh_name], shdr[i].sh_addr);
