db6f1933cb6e878d1349ea358b5ed8f15e7f8a41
tracer-ics2021 <tracer@njuics.org>
2022-12-03 23:07:00 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  23:07:00 up 3 days, 42 min,  1 user,  load average: 1.13, 1.07, 0.95

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -212,11 +212,9 @@ void cpu_exec(uint64_t n)
             puts(s.logbuf);
         }
 #endif
-
-    Log("yes");
-        /*Log("nemu: %s at pc = " FMT_WORD,
+        Log("nemu: %s at pc = " FMT_WORD,
             (nemu_state.state == NEMU_ABORT ? ASNI_FMT("ABORT", ASNI_FG_RED) : (nemu_state.halt_ret == 0 ? ASNI_FMT("HIT GOOD TRAP", ASNI_FG_GREEN) : ASNI_FMT("HIT BAD TRAP", ASNI_FG_RED))),
-            nemu_state.halt_pc);*/
+            nemu_state.halt_pc);
         // fall through
     case NEMU_QUIT:
         statistic();
