ee720b4b64d29bb068c87d552955c1d5b4e74162
tracer-ics2021 <tracer@njuics.org>
2022-12-03 23:06:10 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  23:06:10 up 3 days, 41 min,  1 user,  load average: 1.59, 1.12, 0.96

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -3,14 +3,14 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  for(int i=0;i<32;i++)
+    for(int i=0;i<32;i++)
     //printf("%d %d\n",ref_r->gpr[i]._32,cpu.gpr[i]._32);
-    assert(cpu.gpr[i]._32 == ref_r->gpr[i]._32);
-      //return false;
+    if(cpu.gpr[i]._32 != ref_r->gpr[i]._32)
+        return false;
   //printf("%d %d\n",ref_r->pc,cpu.pc);
-  assert(ref_r->pc==cpu.pc);
-    //return false;
-  return true;
+    if(ref_r->pc != cpu.pc)
+        return false;
+    return true;
 }
 
 void isa_difftest_attach() {
