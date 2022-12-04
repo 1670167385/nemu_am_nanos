ceffa1d2659a2db2f1ffe251617ea226be494c24
tracer-ics2021 <tracer@njuics.org>
2022-12-04 13:30:03 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  13:30:03 up 3 days, 15:05,  1 user,  load average: 0.62, 0.70, 0.87

--- am-kernels-master/tests/am-tests/src/tests/rtc.c
+++ am-kernels-master/tests/am-tests/src/tests/rtc.c
@@ -3,7 +3,7 @@
 void rtc_test() {
   AM_TIMER_RTC_T rtc;
   int sec = 1;
-  while (1) {
+  while (sec<2) {
     //while(io_read(AM_TIMER_UPTIME).us / 1000000 < sec);
     rtc = io_read(AM_TIMER_RTC);
     printf("%d\n",rtc.year);
