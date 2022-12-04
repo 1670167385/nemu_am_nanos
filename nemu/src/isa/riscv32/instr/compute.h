c8a0df91eaa243d8c89efec4ff461266989396eb
tracer-ics2021 <tracer@njuics.org>
2022-12-04 19:02:50 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  19:02:50 up 3 days, 20:38,  1 user,  load average: 0.53, 0.27, 0.17

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -80,6 +80,10 @@ def_EHelper(xori) {
   rtl_xori(s, ddest, dsrc1, id_src2->simm);
 }
 
+def_EHelper(ori) {
+  rtl_ori(s, ddest, dsrc1, id_src2->simm);
+}
+
 def_EHelper(srai) {
   rtl_srai(s, ddest, dsrc1, (id_src2->imm & 0x3f));
 }
