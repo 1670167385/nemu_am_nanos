4d292769c441d011198c8d3006a203b16e49661d
tracer-ics2021 <tracer@njuics.org>
2022-12-05 01:23:35 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  01:23:35 up 4 days,  2:59,  1 user,  load average: 0.75, 0.45, 0.50

--- abstract-machine-master/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine-master/am/src/platform/nemu/ioe/gpu.c
@@ -3,20 +3,40 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+AM_GPU_CONFIG_T config;
+
 void __am_gpu_init() {
+  int i;
+  uint32_t hei_wei = inl(VGACTL_ADDR);
+  int w = config.width = hei_wei >> 16;  
+  int h = config.height = hei_wei & 0xffff;  
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+    config.present = true;
+    config.has_accel = false;
+    config.vmemsz = 0;
+    cfg=&config;
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
+  }
+  uint32_t *fb = (uint32_t *)(uintptr_t)(FB_ADDR+ctl->y*config.width+ctl->x);
+  for(int i=0;i<ctl->h;i++)
+  {
+    for(int j=0;j<ctl->w;j++){
+      if(i+ctl->y < config.height && j+ctl->x < config.width)
+      {
+        *fb = *(uint32_t*)ctl->pixels+i*ctl->w+j;
+      }
+    }
+    fb += (config.width - ctl->w) ;
   }
 }
 
