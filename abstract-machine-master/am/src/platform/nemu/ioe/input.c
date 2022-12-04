76ff7f7685d4e98c296d4bde76243b9db24d9c3f
tracer-ics2021 <tracer@njuics.org>
2022-12-04 23:06:11 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  23:06:11 up 4 days, 41 min,  1 user,  load average: 1.54, 0.83, 0.70

--- abstract-machine-master/am/src/platform/nemu/ioe/input.c
+++ abstract-machine-master/am/src/platform/nemu/ioe/input.c
@@ -5,7 +5,11 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t key = inl(KBD_ADDR);
-  putch('1');
+  int tkey=key;
+  while(tkey){
+    putch('0' + tkey%2);
+    tkey/=2;
+  }
   kbd->keydown = key & KEYDOWN_MASK;
   kbd->keycode = kbd->keydown? (key&(~KEYDOWN_MASK)) : AM_KEY_NONE;
 }
