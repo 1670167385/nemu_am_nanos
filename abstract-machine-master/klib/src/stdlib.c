942a579a92da3fe64c442c5cb82b44fc25ec0f3b
tracer-ics2021 <tracer@njuics.org>
2022-12-04 21:22:14 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  21:22:15 up 3 days, 22:57,  1 user,  load average: 0.73, 0.63, 0.46

--- abstract-machine-master/klib/src/stdlib.c
+++ abstract-machine-master/klib/src/stdlib.c
@@ -34,7 +34,7 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  static char* p=(char*)0x80008000;
+  static char* p=(char*)0x80040000;
   return (void*)(p+=size);
   
 #endif
