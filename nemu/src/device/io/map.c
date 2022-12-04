76b0d910aeb6fa9259546f894201afd109c10f21
tracer-ics2021 <tracer@njuics.org>
2022-12-04 20:15:27 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  20:15:27 up 3 days, 21:50,  1 user,  load average: 0.63, 0.74, 1.21

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -39,6 +39,9 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
+#ifdef CONFIG_DTRACE
+  printf("\33[1;34mDevice reading--%s at addr=0x%x\33[0m",map->name, addr);
+#endif
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
@@ -48,6 +51,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
+#ifdef CONFIG_DTRACE
+  printf("\33[1;34mDevice writing--%s at addr=0x%x\33[0m",map->name, addr);
+#endif
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
