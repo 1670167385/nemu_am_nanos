64ab3d1a4be71d51acaa1397d6912dfcb7de8980
tracer-ics2021 <tracer@njuics.org>
2022-12-05 19:56:33 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  19:56:33 up 4 days, 21:32,  1 user,  load average: 0.97, 1.17, 1.16

--- abstract-machine-master/klib/src/stdlib.c
+++ abstract-machine-master/klib/src/stdlib.c
@@ -28,13 +28,12 @@ int atoi(const char* nptr) {
   }
   return x;
 }
-
+static char* p=(char*)0x80140000;
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  static char* p=(char*)0x80140000;
+#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__)) 
   return (void*)(p+=size);
   
 #endif
