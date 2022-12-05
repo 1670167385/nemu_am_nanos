7bf37dffef4432557f29f45fd6361e08f71cf78d
tracer-ics2021 <tracer@njuics.org>
2022-12-05 11:35:39 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  11:35:39 up 4 days, 13:11,  1 user,  load average: 1.14, 0.87, 0.64

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -57,7 +57,9 @@ static inline void update_screen() {
 #endif
 
 void vga_update_screen(uint32_t offset, int len, bool is_write) {
+  Log("Iam here");
   if(vgactl_port_base[1]&& is_write&& offset==0){
+  Log("Iam in");
     update_screen();
     vgactl_port_base[1] = 0;
   }
