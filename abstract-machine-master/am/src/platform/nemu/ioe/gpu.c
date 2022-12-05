d5e58cbb603197b95f354431a803fea8c3ee5e30
tracer-ics2021 <tracer@njuics.org>
2022-12-05 19:27:12 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  19:27:12 up 4 days, 21:02,  1 user,  load average: 0.83, 0.69, 0.62

--- abstract-machine-master/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine-master/am/src/platform/nemu/ioe/gpu.c
@@ -38,7 +38,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
       //printf("fb start: (%d,%d)  end : ()\n", ctl->y+i, ctl->x ,ctl->y+i);
       for(int j=0;j<ctl->w;j++,fb++){
           *fb = *(uint32_t*)(ctl->pixels+i*ctl->w+j);
-          printf("out:y=%d x=%d  pix:y=%d x=%d addr=%d\n",ctl->y+i,j,i,j,((uint32_t)fb-FB_ADDR));
+          //printf("out:y=%d x=%d  pix:y=%d x=%d addr=%d\n",ctl->y+i,j,i,j,((uint32_t)fb-FB_ADDR));
       }
     }
   }
