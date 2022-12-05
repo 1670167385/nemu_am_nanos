9e647e5537b822f6d1b7e528e63ffec9c9e5b967
tracer-ics2021 <tracer@njuics.org>
2022-12-05 10:51:37 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 10a81b7d9c824e9b9e9127f76e78cee0 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  10:51:38 up 4 days, 12:27,  1 user,  load average: 1.26, 0.96, 0.50

--- abstract-machine-master/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine-master/am/src/platform/nemu/ioe/gpu.c
@@ -10,6 +10,42 @@ void __am_gpu_init() {
   uint32_t hei_wei = inl(VGACTL_ADDR);
   int w = config.width = hei_wei >> 16;  
   int h = config.height = hei_wei & 0xffff;  
+  
+
+
+  int n_d = 1;
+  int tmp_d = w;
+  while(tmp_d>9){
+    n_d*=10;
+    tmp_d/=10;
+  }
+  while(n_d){
+    if(w){
+      putch('0' + w/n_d);
+      w=w%n_d;
+    }
+    else
+      putch('0');
+    n_d/=10;
+  }
+
+  putch(' ');
+  n_d = 1;
+  tmp_d = w;
+  while(tmp_d>9){
+    n_d*=10;
+    tmp_d/=10;
+  }
+  while(n_d){
+    if(h){
+      putch('0' + h/n_d);
+      h=h%n_d;
+    }
+    else
+      putch('0');
+    n_d/=10;
+  }
+
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
