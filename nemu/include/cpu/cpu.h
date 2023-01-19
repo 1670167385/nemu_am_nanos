cdda9bb47dc971822a27b3f2e8d3f9f0ba74b8e0
tracer-ics2021 <tracer@njuics.org>
2023-01-19 19:19:23 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 952ee054d36d4110a95bd5d071e5b77c Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  19:19:23 up  5:26,  1 user,  load average: 1.27, 1.00, 0.77

--- nemu/include/cpu/cpu.h
+++ nemu/include/cpu/cpu.h
@@ -1,10 +1,9 @@
 #ifndef __CPU_CPU_H__
 #define __CPU_CPU_H__
-
+#endif
 #include <common.h>
 
 void cpu_exec(uint64_t n);
 
 void ftrace_call(word_t pc, word_t npc);
 void ftrace_ret(word_t pc, word_t npc);
-#endif
