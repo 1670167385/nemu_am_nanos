d46b8bcb1406a3fc2528b222495bbb7d733fd235
tracer-ics2021 <tracer@njuics.org>
2023-01-19 19:36:25 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 952ee054d36d4110a95bd5d071e5b77c Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  19:36:25 up  5:43,  1 user,  load average: 0.82, 0.77, 0.69

--- nemu/src/cpu/ftrace.c
+++ nemu/src/cpu/ftrace.c
@@ -36,12 +36,11 @@ void init_FTRACE(const char* elf_file)
         fseek(fp, ehdr.e_shoff, SEEK_SET);
     assert(ehdr.e_shoff);
     /* read section header , number is e_shnum */
-    Log("111%d",ehdr.e_shnum);
     shdr = malloc(sizeof(Elf32_Shdr)*ehdr.e_shnum);
-    Log("222");
     assert(shdr);
     fread_ret = fread(shdr, sizeof(Elf32_Shdr), ehdr.e_shnum, fp);
 
+    Log("111%d",ehdr.e_shnum);
     /* read section string table */
     //char *stringtb;
     //stringtb = malloc(sizeof(char)*shdr[ehdr.e_shstrndx].sh_size);
