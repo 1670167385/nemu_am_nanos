7c8e748ed0bb3ab933367e3d200b80485bf45ded
tracer-ics2021 <tracer@njuics.org>
2023-01-20 16:40:18 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 952ee054d36d4110a95bd5d071e5b77c Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  16:40:18 up 1 day,  2:47,  1 user,  load average: 0.67, 0.69, 0.72

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -4,7 +4,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
     /* Trigger an interrupt/exception with ``NO''.
     * Then return the address of the interrupt/exception vector.
     */
-   Log("NO:%d epc: %d",NO,epc);
+   //Log("NO:%d epc: %d",NO,epc);
     cpu.mepc = epc;
     cpu.mcause = NO;
     return cpu.mtvec;
