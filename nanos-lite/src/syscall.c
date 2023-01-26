9334465cb20ad1ec9700636c5e7e9698be795590
tracer-ics2021 <tracer@njuics.org>
2023-01-26 16:04:42 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 0acb4249effb4b44a4b8772b3f2a6f9d Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-58-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  16:04:42 up  4:37,  1 user,  load average: 0.73, 0.79, 0.71

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,22 +1,23 @@
 #include <common.h>
 #include "syscall.h"
-//#define CONFIG_STRACE
+#define CONFIG_STRACE
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    case 0: //exit
+    case SYS_exit:
 #ifdef CONFIG_STRACE 
-      Log("syscall exit start with a0=0x%x", c->GPR1); 
+      Log("syscall exit start with a0=0x%x", c->GPR2); 
       sys_exit(c);
       Log("syscall exit end"); //, ret=0x%x", c->GPRx)); 
 #else
       sys_exit(c);
 #endif
       break;
-    case 1: //yield
+    
+    case SYS_yield: 
 #ifdef CONFIG_STRACE
       Log("syscall yield start"); 
       sys_yield(c); 
@@ -26,11 +27,29 @@ void do_syscall(Context *c) {
 #endif
       break;
 
+    case SYS_write:
+#ifdef CONFIG_STRACE
+      Log("syscall write start with a0=0x%x a1=0x%x a2=0x%x", c->GPR2, c->GPR3, c->GPR4); 
+      sys_write(c); 
+      Log("syscall write end, ret=0x%x", c->GPRx); 
+#else
+      sys_write(c); 
+#endif
+
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
 
+void sys_write(Context *c)
+{
+  if(c->GPR2==1 || c->GPR2==2){  //out/err
+    for(int p=0; p < c->GPR4; p++){
+      putch(*(char*)(c->GPR3 + p));
+    }
+    c->GPRx = c->GPR4;  //return count;
+  }
+}
 
 void sys_yield(Context *c){
   yield();
