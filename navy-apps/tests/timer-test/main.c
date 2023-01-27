3b6e0b5f7bd86ae0fdffcf94b90b603726bb4a9e
tracer-ics2021 <tracer@njuics.org>
2023-01-27 21:28:09 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 753b16a2ffe8420faa32965a8ea50046 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-58-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  21:28:09 up  5:18,  1 user,  load average: 0.41, 0.70, 0.80

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -9,7 +9,7 @@ int main() {
   while(1){
     _gettimeofday(&t, NULL);
     if(t.microsec == 500 && t.sec != lt.sec){
-      printf("%d:%d Hello world!\n", t.sec, t.microsec);
+      printf("%ld:%ld Hello world!\n", t.sec, t.microsec);
       lt.sec = t.sec;
     }
   }
