2c01f0529f92e8fc28fce84efe81d01962960cf0
tracer-ics2021 <tracer@njuics.org>
2023-01-31 23:35:10 +0800

    >  compile 2054343  Static hostname: xsr-Lenovo-Legion-R7000P2020H        Icon name: computer-laptop          Chassis: laptop       Machine ID: 6b0fafce6b65411598b2e7389dc32119          Boot ID: 753b16a2ffe8420faa32965a8ea50046 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-58-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R7000P2020H  23:35:10 up 4 days,  7:25,  1 user,  load average: 1.70, 1.04, 0.66

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -9,10 +9,18 @@
 int main() {
   fixedpt a = fixedpt_rconst(1.2);
   fixedpt b = fixedpt_fromint(10);
-  int c = 0;
-  if (b > fixedpt_rconst(7.9)) {
-    c = fixedpt_toint(fixedpt_div(fixedpt_mul(a + FIXEDPT_ONE, b), fixedpt_rconst(2.3)));
-  }
-  printf("%d\n", c);
+
+
+  a = fixedpt_rconst(-1.3);
+  printf("-1.3: flo:%d cei:%d\n", fixedpt_floor(a), fixedpt_ceil(a));
+  
+  a = fixedpt_rconst(-0.2);
+  printf("-0.2: flo:%d cei:%d\n", fixedpt_floor(a), fixedpt_ceil(a));
+
+  a = fixedpt_rconst(1.3);
+  printf("1.3: flo:%d cei:%d\n", fixedpt_floor(a), fixedpt_ceil(a));
+  
+  a = fixedpt_rconst(0.2);
+  printf("0.2: flo:%d cei:%d\n", fixedpt_floor(a), fixedpt_ceil(a));
   return 0;
 }
